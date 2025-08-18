c729d9e7eb71c24eb3f7fc1cfd7cef55a788b661
jizero <163485562+jiyoungLLL@users.noreply.github.com>
2025-08-18 17:46:29 +0900

    refactor: 이메일 인증 요청 버튼 클릭시 인증번호 입력 칸 open props 추가

--- src/components/signup/EmailConfirmInput.tsx
+++ src/components/signup/EmailConfirmInput.tsx
@@ -7,12 +7,18 @@ import Input from '../ui/Input';
 
 type ConfirmProps = {
   email: string;
+  setConfirmEmail: (value: boolean) => void;
+  setOpenConfirm: (value: boolean) => void;
 };
 
 const CodeOnlySchema = ConfirmVerifyEmailSchema.pick({ code: true });
 type CodeForm = z.infer<typeof CodeOnlySchema>;
 
-export default function EmailConfirmInput({ email }: ConfirmProps) {
+export default function EmailConfirmInput({
+  email,
+  setConfirmEmail,
+  setOpenConfirm,
+}: ConfirmProps) {
   const { control, handleSubmit } = useForm<CodeForm>({
     resolver: zodResolver(CodeOnlySchema),
     mode: 'onChange',
@@ -20,34 +26,37 @@ export default function EmailConfirmInput({ email }: ConfirmProps) {
     shouldFocusError: true,
   });
 
-  const confirmCode = useConfirmEmailCode();
+  const onSuccess = () => {
+    setConfirmEmail(true);
+    setOpenConfirm(false);
+  };
+  const confirmCode = useConfirmEmailCode({ onSuccess });
 
   const onSubmit = ({ code }: CodeForm) => {
     confirmCode.mutate({ email, code });
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className='flex items-start gap-[15px] justify-center'>
-        <div className='flex-1'>
-          <Input<CodeForm>
-            name='code'
-            control={control}
-            placeholder='인증번호 6자리'
-            type='text'
-            showMaxLength={false}
-            maxLength={6}
-          />
-        </div>
-
-        <button
-          type='submit'
-          className='w-[70px] h-[50px] text-[#ffffff] bg-blue-50 font-semibold rounded-[10px] disabled:bg-gray-30'
-          disabled={confirmCode.isPending}
-        >
-          확인
-        </button>
+    <div className='flex items-start gap-[15px] justify-center'>
+      <div className='flex-1'>
+        <Input<CodeForm>
+          name='code'
+          control={control}
+          placeholder='인증번호 6자리'
+          type='text'
+          showMaxLength={false}
+          maxLength={6}
+        />
       </div>
-    </form>
+
+      <button
+        type='submit'
+        onClick={handleSubmit(onSubmit)}
+        className='w-[70px] h-[50px] text-[#ffffff] bg-blue-50 font-semibold rounded-[10px] disabled:bg-gray-30'
+        disabled={confirmCode.isPending}
+      >
+        확인
+      </button>
+    </div>
   );
 }
