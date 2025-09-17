20a472970959f1743a30a2fe6fc94c3c19a8a80b
jizero <163485562+jiyoungLLL@users.noreply.github.com>
2025-09-18 02:49:36 +0900

    refactor: demo 훅 수정

---
+++ src/hooks/queries/dashboard/test-demo/useCheckHealth.ts
@@ -0,0 +1,15 @@
+import { useApiQuery } from '../../useApi';
+
+type TestHealth = {
+  status: string;
+  timestamp: string;
+  sevice: string;
+};
+
+export const useCheckHealth = () =>
+  useApiQuery<TestHealth>({
+    queryKey: ['test', 'health'],
+    queryOptions: {
+      endpoint: '/proxy/test/health',
+    },
+  });
